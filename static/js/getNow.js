b96b25963509d9fce798e413cfecefd1111be718
naoki-urabe <na0k1ura6e@gmail.com>
2020-03-08 21:38:37 +0900

    add zeropadding to getNow.js

--- static/js/getNow.js
+++ static/js/getNow.js
@@ -1,8 +1,8 @@
 function getNow(){
     var nowTime = new Date();
-    var nowHour = nowTime.getHours();
-    var nowMin = nowTime.getMinutes();
-    var nowSec = nowTime.getSeconds();
+    var nowHour = ('0' + nowTime.getHours()).slice(-2);
+    var nowMin = ('0' + nowTime.getMinutes()).slice(-2);
+    var nowSec = ('0' + nowTime.getSeconds()).slice(-2);
     var msg = nowHour+":"+nowMin+":"+nowSec
     document.getElementById("Clock").innerHTML = msg
     return msg
