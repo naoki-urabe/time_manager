21f0a1949ee367e3a9be234b93b4f0262e0d9fd8
naoki-urabe <na0k1ura6e@gmail.com>
2020-03-22 22:06:44 +0900

    画面表示整形と活動時間計算

--- activity_record/views.py
+++ activity_record/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from .models import ActiveRecord
 from .models import TaskRecord
 from django.utils import timezone
+from django.utils.timezone import localtime
 import datetime
 
 # Create your views here.
@@ -10,11 +11,13 @@ class ActivityRecordView(View):
     def get(self, request, *args, **kwargs):
         active_exists = self.get_active_exists()
         active_id = self.get_active_id(active_exists)
+        active_histories = self.get_active_histories()
         task_exists = self.get_task_exists()
         task_id = self.get_task_id(task_exists)
         task_name = self.get_task_name(task_id)
+        task_histories = self.get_task_histories()
         active_status = '活動中' if (active_exists) else '睡眠中'
-        task_status = '進行中' if (task_exists) else '開始'
+        task_status = '終了' if (task_exists) else '開始'
         context = {
             'active_exists': active_exists,
             'active_id': active_id,
@@ -22,20 +25,29 @@ class ActivityRecordView(View):
             'task_exists': task_exists,
             'task_id': task_id,
             'task_name': task_name,
-            'task_status': task_status
+            'task_status': task_status,
+            'task_histories': task_histories,
+            'today_activity': active_histories
         }
         return render(request, 'activity_record.html', context)
     def get_active_exists(self):
         active_exists = ActiveRecord.objects.filter(is_active=True).exists()
         return active_exists
+    def get_active_histories(self):
+        active_histories = ActiveRecord.objects.filter(today_jst=localtime(timezone.now()))
+        return active_histories
     def get_active_id(self,active_exists):
         active_id = -1
         if active_exists:
-            active_id =  ActiveRecord.objects.get(task='active',today=timezone.now().date(),is_active=True).id
+            print(timezone.now())
+            active_id =  ActiveRecord.objects.get(task='active',today_jst=to_jst(timezone.now()),is_active=True).id
         return active_id
     def get_task_exists(self):
         task_exists = TaskRecord.objects.filter(is_active=True).exists()
         return task_exists
+    def get_task_histories(self):
+        task_histories = TaskRecord.objects.filter(today_jst=localtime(timezone.now()))
+        return task_histories
     def get_task_id(self,task_exists):
         task_id = -1
         if task_exists:
@@ -51,12 +63,15 @@ class RegisterActiveRecord(View):
         active_exists=request.POST['active_exists']
         if active_exists == 'True':
             active_record = ActiveRecord.objects.get(is_active=True)
-            active_record.end_time=timezone.now()
+            active_record.end_time=localtime(timezone.now())
+            #print(task_record.end_time-task_record.begin_time)
             #task_record.period=task_record.end_time-task_record.begin_time
+            active_record.period = timedelta_to_sec(active_record.end_time - active_record.begin_time)
             active_record.is_active=False
+            active_record.today_jst = to_jst(timezone.now())
             active_record.save()
         else:
-            active_record = ActiveRecord(task='active',begin_time=timezone.now())
+            active_record = ActiveRecord(task='active',begin_time=localtime(timezone.now()),today_jst=to_jst(timezone.now()))
             active_record.save()
         context = {
             'active_status': "打刻完了",
@@ -65,23 +80,30 @@ class RegisterActiveRecord(View):
         return render(request, 'register_active_record.html', context)
 class RegisterTaskRecord(View):
     def post(self, request, *args, **kwargs):
-        print("test")
         task_id=request.POST['task_id']
         if task_id=='-1':
             task_name=request.POST['task_name']
-            task_record = TaskRecord(task=task_name,begin_time=timezone.now())
+            task_record = TaskRecord(task=task_name,begin_time=localtime(timezone.now()),today_jst=to_jst(timezone.now()))
             task_record.save()
         else:
             task_record = TaskRecord.objects.get(id=task_id)
-            task_record.end_time=timezone.now()
-            #task_record.period = task_record.end_time - task_record.begin_time
+            task_record.end_time=localtime(timezone.now())
+            print(type(task_record.end_time-task_record.begin_time))
+            task_record.period = timedelta_to_sec(task_record.end_time - task_record.begin_time)
             task_record.is_active = False
+            task_record.today_jst = to_jst(timezone.now())
             task_record.save()
         context = {
             'active_status': "打刻完了",
             'now': timezone.now()
         }
         return render(request, 'register_task_record.html', context)
+def to_jst(time):
+    print((time + datetime.timedelta(hours=9)).date())
+    return (time + datetime.timedelta(hours=9)).date()
+def timedelta_to_sec(timedelta):
+    sec = timedelta.days*86400 + timedelta.seconds
+    return sec
 activity_record = ActivityRecordView.as_view()
 register_active_record = RegisterActiveRecord.as_view()
 register_task_record = RegisterTaskRecord.as_view()
