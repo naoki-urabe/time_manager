c61098ea9f5914eb922feebb27100fbf5c8c7876
naoki-urabe <na0k1ura6e@gmail.com>
2020-03-28 16:56:10 +0900

    リファクタリング

--- activity_record/models.py
+++ activity_record/models.py
@@ -6,16 +6,8 @@ class ActiveRecord(models.Model):
     task = models.CharField(verbose_name='タスク名', max_length=50)
     begin_time = models.fields.DateTimeField(null=True)
     end_time = models.fields.DateTimeField(null=True)
-    period = models.fields.FloatField(null=True)
+    #period = models.fields.FloatField(null=True)
     today = models.fields.DateTimeField(default = timezone.now())
     today_jst = models.fields.DateField()
     is_active = models.fields.BooleanField(default = True)
-
-class TaskRecord(models.Model):
-    task = models.CharField(verbose_name='タスク名', max_length=50)
-    begin_time = models.fields.DateTimeField(null=True)
-    end_time = models.fields.DateTimeField(null=True)
-    period = models.fields.FloatField(null=True)
-    today = models.fields.DateTimeField(default = timezone.now())
-    today_jst = models.fields.DateField()
-    is_active = models.fields.BooleanField(default = True)
+    active_type = models.CharField(verbose_name='タスク名', max_length=10)
