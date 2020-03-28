95431001d85d430900d145f432c62435f4064829
naoki-urabe <na0k1ura6e@gmail.com>
2020-03-28 21:42:50 +0900

    columnをnull可に変更

--- activity_record/models.py
+++ activity_record/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 
 # Create your models here.
 class ActiveRecord(models.Model):
-    task = models.CharField(verbose_name='タスク名', max_length=50)
+    task = models.CharField(verbose_name='タスク名', max_length=50,null=True)
     begin_time = models.fields.DateTimeField(null=True)
     end_time = models.fields.DateTimeField(null=True)
     period = models.fields.FloatField(null=True)
-    today = models.fields.DateTimeField()
-    today_jst = models.fields.DateField()
+    today = models.fields.DateTimeField(null=True)
+    today_jst = models.fields.DateField(null=True)
     is_active = models.fields.BooleanField(default = True)
-    active_type = models.CharField(verbose_name='タスク名', max_length=10)
+    active_type = models.CharField(verbose_name='タスク名', max_length=10,null=True)
