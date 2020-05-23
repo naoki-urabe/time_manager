38010ba5ffb41837032266c643becd0d1c618e85
naoki-urabe <na0k1ura6e@gmail.com>
2020-05-23 22:00:42 +0900

    forms切り分け

--- activity_record/views/subject_log_view.py
+++ activity_record/views/subject_log_view.py
@@ -10,13 +10,6 @@ from django.utils import timezone
 from django.utils.timezone import localtime
 import datetime
 from time import mktime
-from activity_record.forms import ActiveRecordFormSet
-from activity_record.forms import ActiveRecordForm
-from activity_record.forms import SubjectFormSet
-from activity_record.forms import GearFormSet
-from activity_record.forms import ReviewFormSet
-import re
-from activity_record.modules import module
 
 class SubjectLogView(View):
     def get(self, request, *args, **kwargs):
