c2c23e6d3e34a0dee9252c02ac6efd7672b1f88a
naoki-urabe <na0k1ura6e@gmail.com>
2020-07-11 08:56:27 +0000

    github actions テスト

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -3,7 +3,8 @@
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
-
+    host_port = "$DATABASE_HOST $DATABASE_PORT"
+    echo host_port
     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
       sleep 0.1
     done
